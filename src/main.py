bab47b5677b904f4cbdeeba52f2700e4c79668ad
Ferriit <ferriitalt@gmail.com>
2026-01-18 17:05:51 +0100

    added visualization with pygame and made the C++ stuff compatible with python

--- src/main.py
+++ src/main.py
@@ -1,3 +1,66 @@
 from modules import sim
+import pygame as pg
+import random
+import math
+
+pg.init()
+
+WIDTH, HEIGHT = 800, 600
+BOUNDS = 200, 150
+
+WR, HR = WIDTH / BOUNDS[0], HEIGHT / BOUNDS[1]
+
+AGENTAMOUNT = 1500
+CITYAMOUNT = 20
+
+ctx = pg.display.set_mode((WIDTH, HEIGHT));
+clock = pg.time.Clock()
+run = True;
+
+def pos(x, y):
+    p = sim.pos()
+    p.x = x
+    p.y = y
+    return p
+
+s = sim.sim()
 
 
+def polarToCartesian(r, theta):
+    return pos(r * math.cos(theta), r * math.sin(theta))
+
+
+def generateAgents():
+    populationcenters = []
+    for _ in range(CITYAMOUNT):
+        populationcenters.append(pos(random.randint(0, BOUNDS[0]), random.randint(0, BOUNDS[1])))
+
+    for _ in range(AGENTAMOUNT):
+        if random.randint(0, 1) == 1:
+            s.addIndividual(pos(random.randint(0, BOUNDS[0]), random.randint(0, BOUNDS[1])))
+        else:
+            r, theta = abs(random.gauss(0, 7)), random.uniform(0, 2 * math.pi)
+
+            city = random.randrange(0, CITYAMOUNT)
+
+            position = polarToCartesian(r, theta);
+            position.x += populationcenters[city].x
+            position.y += populationcenters[city].y
+
+            s.addIndividual(position)
+
+generateAgents()
+
+while run:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            run = False
+    
+    ctx.fill((0, 0, 0));
+    for i in s.individuals:
+        pg.draw.aacircle(ctx, (255, 255, 255), (i.x * WR, i.y * HR), 2)
+
+    pg.display.flip()
+
+    clock.tick(60)
--- src/sim.cpp
+++ src/sim.cpp
@@ -1,11 +1,44 @@
-#include <iostream>
-#include <string>
+#include <vector>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
-void print(std::string text) {
-    std::cout << text << std::endl;
-}
+namespace py = pybind11;
+
+struct pos {
+    float x, y;
+    pos() : x(0), y(0) {}
+    pos(float x_, float y_) : x(x_), y(y_) {}
+};
+
+class sim {
+    public:
+        std::vector<pos> individuals;
+
+        void addIndividual(pos position) {
+            individuals.push_back(position);
+        }
+
+        std::vector<pos> fetchIndividuals() {
+            return individuals;
+        }
+};
 
 PYBIND11_MODULE(sim, m) {
-    m.def("print", &print);
+    py::class_<pos>(m, "pos")       // Treat struct as class
+        .def(py::init<>())
+        .def(py::init<float,float>())
+        .def_readwrite("x", &pos::x)
+        .def_readwrite("y", &pos::y)
+        .def("__str__", [](const pos &p) {
+            return "(" + std::to_string(p.x) + ", " + std::to_string(p.y) + ")";
+        })
+        .def("__repr__", [](const pos &p) {
+            return "pos(x=" + std::to_string(p.x) + ", y=" + std::to_string(p.y) + ")";
+        });
+
+    py::class_<sim>(m, "sim")
+        .def(py::init<>())
+        .def_readwrite("individuals", &sim::individuals)
+        .def("addIndividual", &sim::addIndividual)
+        .def("fetchIndividuals", &sim::fetchIndividuals);
 }
